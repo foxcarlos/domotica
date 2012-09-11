5fd060d4673affb45a9796e05447e4fd7ad06efe
foxcarlos <foxcarlos@gmail.com>
2012-09-10 21:23:58 -0430

    new file:   enviar_inf_puerto.ino         modified:   puerduino.py

--- puerduino.py
+++ puerduino.py
@@ -1,10 +1,12 @@
 import serial
+import time
 
 def main():
     while 1:
         s = serial.Serial("/dev/ttyACM1")
         recv = s.readline()
         print recv.split()
+        time.sleep(2)
 
 if __name__ == '__main__':
     main()
