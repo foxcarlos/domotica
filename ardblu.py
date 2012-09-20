3d8c039a1a66fc5d5d18d100b484c93db5013628
foxcarlos <foxcarlos@gmail.com>
2012-09-20 15:39:22 -0430

    Cambios varios

--- ardblu.py
+++ ardblu.py
@@ -6,15 +6,15 @@ import serial
 
 salir = False
 mi_telefono = 'AC:E8:7B:6E:51:9C'
-s = serial.Serial("/dev/ttyACM0")
+s = serial.Serial("/dev/ttyACM2")
 
 while not salir:
-    encontrados = bluetooth.discover_devices(duration=4, flush_cache=True, lookup_names=False)
+    encontrados = bluetooth.discover_devices(duration=3, flush_cache=True, lookup_names=False)
     if mi_telefono in encontrados:
-        print '0 Apagar Led'
+        print '1 Abrir Hembrilla'
         #os.system('gnome-screensaver-command -d')
-        s.write('0')
-    else:
-        print '1 Encender Led'
-        #os.system('gnome-screensaver-command -l')
         s.write('1')
+    else:
+        print '0 No Abrir nada'
+        #os.system('gnome-screensaver-command -l')
+        s.write('0')
